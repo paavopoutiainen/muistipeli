89ad10ccf315373995c213c094cd1a1e02e0acca
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-07-25 23:23:59 +0300

    Added points counter, added some comments, modified layout

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-
-
 var correctUpper = [];
 var correctLower = [];
 var sizeOfTheGrid = 4;
 var numberOfGreens = 5;
+var points = 0;
 
 const createGameGrids = () => {
-  //Getting divs into which blocks will be created at
+  //Getting divs into which grid of blocks will be created at
   const gameGridDivsHTMLCollection = document.getElementsByClassName("gameGrid")
   var arr = Array.prototype.slice.call( gameGridDivsHTMLCollection )
   console.log("hehehehe")
@@ -19,7 +18,7 @@ const createGameGrids = () => {
   
     for(let i = 0; i < sizeOfTheGrid * sizeOfTheGrid; i++) {
       let gridItem = parent.document.createElement("div");
-      var classList = div.className.split(/\s+/);
+      let classList = div.className.split(/\s+/);
       if(classList.includes("answer")){
         gridItem.className += `${div.id} gameGridItem`
         gridItem.onclick = () => handleRightSideClick(i, gridItem)
@@ -33,13 +32,28 @@ const createGameGrids = () => {
   })
 }
 
-const checkIfgreenBlocksLeft = (numberOfGreens, numberOfGivenGreens) => {
-  if(numberOfGivenGreens < numberOfGreens) {
-    return true;
-  } else return false;
+/*this function creates an array of booleans to be used in deciding which blocks will be 
+colored green during the game*/
+const getRandomizedGreensArray = () => { 
+  const array = [];
+  const numberOfBlocksInTheGrid = sizeOfTheGrid ** 2;
+  let numberOfGivenGreens = 0
+  for (i = 0; i < numberOfBlocksInTheGrid; i++) {
+    /*Creating proper ratio for randomizing greens*/
+    const ratio = numberOfGreens / numberOfBlocksInTheGrid;
+    const greenOrNotBoolean = !(Math.random() >= ratio);
+    //Checking if green has to be given at certain loop round and also checking excessive greens are not given
+    if(checkIfGreenHasToBeGiven(i, numberOfGreens, numberOfGivenGreens, numberOfBlocksInTheGrid) || (greenOrNotBoolean && checkIfGreenCanBeGiven(numberOfGreens, numberOfGivenGreens))){
+      array.push({green: true, checked: false})
+      numberOfGivenGreens++
+    } else {
+      array.push({green: false, checked: false})
+    }  
+  }
+  return array
 }
 
-const checkIfgreenHasToBeGiven = (index, numberOfGreens, numberOfGivenGreens, numberOfBlocksInTheGrid) => {
+const checkIfGreenHasToBeGiven = (index, numberOfGreens, numberOfGivenGreens, numberOfBlocksInTheGrid) => {
   const indexesLeft = numberOfBlocksInTheGrid - index;
   const numberOfGreensYetToBeGiven = numberOfGreens - numberOfGivenGreens;
   if(numberOfGreensYetToBeGiven === indexesLeft) {
@@ -47,42 +61,23 @@ const checkIfgreenHasToBeGiven = (index, numberOfGreens, numberOfGivenGreens, nu
   } else return false;
 }
 
-//this function creates an array of booleans to be used in deciding which blocks will be 
-//cologreen green during the game
-const getRandomizedGreensArray = (gridRows, numberOfGreens) => { 
-  
-  const array = [];
-  const numberOfBlocksInTheGrid = gridRows ** 2;
-  let numberOfGivenGreens = 0
-  for (i = 0; i < numberOfBlocksInTheGrid; i++) {
-    const ratio = numberOfGreens / numberOfBlocksInTheGrid;
-    const greenOrNotBoolean = !(Math.random() >= ratio);
-    if(checkIfgreenHasToBeGiven(i, numberOfGreens, numberOfGivenGreens, numberOfBlocksInTheGrid) || (greenOrNotBoolean && checkIfgreenBlocksLeft(numberOfGreens, numberOfGivenGreens))){
-      array.push(true)
-      numberOfGivenGreens++
-    } else {
-      array.push(false)
-    }  
-  }
-  return array
+const checkIfGreenCanBeGiven = (numberOfGreens, numberOfGivenGreens) => {
+  if(numberOfGivenGreens < numberOfGreens) {
+    return true;
+  } else return false;
 }
 
-const showGreens = (blockArray, correctArray) => {
-  blockArray.forEach((block, i) => {
-    const greenOrNotBoolean = correctArray[i]
-    if (greenOrNotBoolean){
-      block.style.backgroundColor = "green"
-    }
-  })
-} 
+//Handling play game button click
+const playGame = async () => {
+  //Filling global arrays of 
+  correctUpper = getRandomizedGreensArray();
+  correctLower = getRandomizedGreensArray();
 
-const setBackToWhites = (blockArray) => {
-  blockArray.forEach((block) => {
-    block.style.backgroundColor = "white"
-  })
+  await handleLeftSide();
+  showRightSideHideLeftSide();
 }
 
-const handleLeftSide = (correctUpper, correctLower) => {
+const handleLeftSide = () => {
   const upperLeftDiv = document.getElementById("upperLeft")
   const upperLeftBlockNodes = upperLeftDiv.childNodes
   const upperLeftBlocksArray = Array.from(upperLeftBlockNodes)
@@ -106,6 +101,21 @@ const handleLeftSide = (correctUpper, correctLower) => {
   })
 }
 
+const showGreens = (blockArray, correctArray) => {
+  blockArray.forEach((block, i) => {
+    const greenOrNotBoolean = correctArray[i].green
+    if (greenOrNotBoolean){
+      block.style.backgroundColor = "green"
+    }
+  })
+} 
+
+const setBackToWhites = (blockArray) => {
+  blockArray.forEach((block) => {
+    block.style.backgroundColor = "white"
+  })
+}
+
 const showRightSideHideLeftSide = () => {
   const rightSideDivsHTMLCollection = document.getElementsByClassName("answer")
   const rightSideDivsArray = Array.from(rightSideDivsHTMLCollection)
@@ -121,27 +131,45 @@ const showRightSideHideLeftSide = () => {
   })
 }
 
-const playGame = async () => {
-  //eli tässä vain luotais ne vihreät 
-  correctUpper = getRandomizedGreensArray(sizeOfTheGrid, numberOfGreens);
-  correctLower = getRandomizedGreensArray(sizeOfTheGrid, numberOfGreens);
-
-  await handleLeftSide(correctUpper, correctLower)
-  showRightSideHideLeftSide()
-}
-
+/*Check if clicked block was green or not and give points accordingly */
 const handleRightSideClick = (idNumber, gridItem) => {
-  const classList = gridItem.className.split(/\s+/)
+  
+  //Figure out which grid was blocked 
+  const classList = gridItem.className.split(/\s+/);
   const correctArray = classList.includes("upperRight") ? correctUpper : correctLower;
-  //tsekkaa oliko klikatun blockin id true vai false, jos true niin muuta väri vihreäksi jos 
-  //false niin muuta väri punaiseksi
-  if(correctArray[idNumber]) {
-    gridItem.style.backgroundColor = "green"
+  const blockInfo = correctArray[idNumber]
+  if(blockInfo.green) {
+    gridItem.style.backgroundColor = "green";
   } else {
-    gridItem.style.backgroundColor = "red"
+    gridItem.style.backgroundColor = "red";
+  }
+  givePoints(blockInfo, correctArray, idNumber)
+  if(points === numberOfGreens * 2){
+    disableOnClickEvents()
+    showResults()
   }
 }
 
+const disableOnClickEvents = () => {
+  const gameGridItems = document.getElementsByClassName("gameGridItem")
+  const gameGridItemsArray = Array.from(gameGridItems)
+  gameGridItemsArray.forEach(block => {
+    block.onclick = "";
+  })
+}
+
+const givePoints = (blockInfo, correctArray, idNumber) => {
+  if(!blockInfo.checked) {
+    correctArray[idNumber].checked = true
+    if(blockInfo.green) {
+      points++;
+    } else {
+      points--;
+    }
+  } 
+}
+
+//Handling user's changes in preferences
 const changeGridSize = () => {
   const newValue = document.getElementById("sizeOfTheGridSelect").value
   const newValueAsNumber = parseInt(newValue)
@@ -155,5 +183,5 @@ const changeNumberOfGreens = () => {
   numberOfGreens = newValueAsNumber
 }
 
-createGameGrids(sizeOfTheGrid);
+createGameGrids();
 
