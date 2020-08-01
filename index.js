e5c4e249d56e89934ad273ce3720e78e83c4c93e
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-08-01 17:25:48 +0300

    Changed foldername public into client

--- index.js
+++ index.js
@@ -6,18 +6,9 @@ const path = require('path');
 const port = process.env.PORT || 3001
 
 app.use(cors())
-app.use(express.static('public'));
-app.use('/cssFiles', express.static('public'))
-app.use('/scripts', express.static('public'))
+app.use(express.static('client'));
+app.use('/cssFiles', express.static('client'))
+app.use('/scripts', express.static('client'))
 
-/*
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'))
-})
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'))
-})
-*/
 const server = app.listen(port, () => console.log(`Muistipeli app listening on port ${port}!`))
 
